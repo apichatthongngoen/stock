fcaecc4715bd1bb7cc302f6e782d5c85108da928
apichatthongngoen <apichat_thongngoen@outlook.com>
2019-11-19 15:34:10 +0700

    add  page login

---
+++ login.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script type="text/javascript" src="inc/autocomplete.js"></script>
+    <link rel="stylesheet" href="inc/autocomplete.css"  type="text/css"/>
+    <title>STOCK OFFICE เบิกพัสดุ </title>
+  </head>
+  <body>
+    <div class="container">
+
+<br>
+    <?php
+    $code=$_GET['code'];
+
+    echo"
+    <a href=\"index.php?code=$code\" class=\"btn btn-primary btn-lg\">LOGIN</a>
+    
+    ";
+    echo'
+
+    </div>
+    
+    
+    
+    ';
