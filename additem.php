cb3fedb21d4d4fac58e645ef44fb12a5385ccc8a
apichatthongngoen <apichat_thongngoen@outlook.com>
2019-11-21 17:00:24 +0700

    ซื้อพัสดุเข้า
    เพิ่มหน้า ซื้อพัสดุเข้า

---
+++ additem.php
@@ -0,0 +1,88 @@
+<?php
+require_once 'inc/dbconnect.php';
+//require_once 'inc/c_login.php';
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script type="text/javascript" src="inc/autocomplete.js"></script>
+    <link rel="stylesheet" href="inc/autocomplete.css"  type="text/css"/>
+    <title>STOCK OFFICE เบิกพัสดุ </title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <h3>ซื้อพัสดุเข้า</h3>
+        </div>
+
+        <div class="row justify-content-md-center">
+            <div class="col-md-auto center">
+                <form id="form1" name="form1" method="post" action="inc/fn.php">
+                    <div class="input-group input-group-sm mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-sm">พิมพ์ชื่อพัสดุ</span>
+                        </div>
+                    <input type="text" size="50" class="form-control" name="show_province" id="show_province" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                    <input class="form-control" name="h_province_id" type="hidden" id="h_province_id" value="" />
+                    </div>
+                    <div class="input-group input-group-sm mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-sm">จำนวน</span>
+                        </div>
+                        <input class="form-control" type="number" id="number" name="number" placeholder="0-99" value="" />
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" name="btn_submit_add" id="btn_submit_add" value="1" class="btn btn-warning">ซื้อพัสดุเข้า</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+        <div class="row justify-content-md-center">
+            <h3> <?php 
+                if (isset($_GET['product']) && $_GET['product'] != "") {
+                    $product=$_GET['product'];
+                    $sql = "SELECT * FROM products WHERE id = $product ORDER BY id DESC LIMIT 2";
+                    $qr = select($sql);
+                    $rs = $qr[0]; 
+                    echo "".$rs['name']."/ คงเหลือ : ".$rs['sku']." ชิ้น";
+                }
+                if (isset($_GET['error']) && $_GET['error'] != "") {
+                    if ($_GET['error']==1) {
+                        echo "เบิกเกินจำนวนที่เหลืออยู่ ";
+                        # code...
+                    }
+                }
+                ?>
+            </h3>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript">
+        function make_autocom(autoObj,showObj){
+            var mkAutoObj=autoObj;
+            var mkSerValObj=showObj;
+            new Autocomplete(mkAutoObj, function() {
+                this.setValue = function(id) {
+                    document.getElementById(mkSerValObj).value = id;
+                }
+                if ( this.isModified )
+                    this.setValue("");
+                if ( this.value.length < 1 && this.isNotClick )
+                    return ;
+                return "inc/gdata.php?q="+encodeURIComponent(this.value);
+            });
+        }
+        // การใช้งาน
+        // make_autocom(" id ของ input ตัวที่ต้องการกำหนด "," id ของ input ตัวที่ต้องการรับค่า");
+        make_autocom("show_province","h_province_id");
+    </script>
+    </body>
+</html>
--- inc/fn.php
+++ inc/fn.php
@@ -11,6 +11,7 @@ if (isset($_POST['btn_submit_index']) && $_POST['btn_submit_index'] != "") {
         "product_id" => "$product_id",
         "sku" => "$number",
         "date" => "$dt",
+        "rate" => "del",
     );
     $sql = "SELECT * FROM products WHERE id = $product_id ORDER BY id DESC LIMIT 2";
     $qr = select($sql);
@@ -23,11 +24,41 @@ if (isset($_POST['btn_submit_index']) && $_POST['btn_submit_index'] != "") {
         "date_time" => "$dt",
     );
     if ($number_produuct_sum >= 0) {
-        //insert("orders_item", $data);
-        //update("products", $data2, "id=$product_id");
+        insert("orders_item", $data);
+        update("products", $data2, "id=$product_id");
         header("location: ../index.php?product=$product_id");
     } else {
         header("location: ../index.php?error=1");
+    }
+
+}
+if (isset($_POST['btn_submit_add']) && $_POST['btn_submit_add'] != "") {
+    $dt = new DateTime();
+    $product_id = $_POST['h_province_id'];
+    $number = $_POST['number'];
+    $dt = $dt->format('Y-m-d H:i:s');
+    $data = array(
+        "product_id" => "$product_id",
+        "sku" => "$number",
+        "date" => "$dt",
+        "rate" => "add",
+    );
+    $sql = "SELECT * FROM products WHERE id = $product_id ORDER BY id DESC LIMIT 2";
+    $qr = select($sql);
+    $rs = $qr[0];
+    $number_produuct = $rs['sku'];
+    $number_produuct_sum = $number_produuct + $number;
+    echo $number_produuct_sum;
+    $data2 = array(
+        "sku" => "$number_produuct_sum",
+        "date_time" => "$dt",
+    );
+    if ($number_produuct_sum >= 0) {
+        insert("orders_item", $data);
+        update("products", $data2, "id=$product_id");
+        header("location: ../additem.php?product=$product_id");
+    } else {
+        header("location: ../additem.php?error=1");
     }
 
 }
